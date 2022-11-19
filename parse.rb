659251954bba35dda8021f10246bbaae514d2c07
digitaldiver <1119han@gmail.com>
2022-11-19 14:45:45 +0100

    add unique counts for each url

--- parse.rb
+++ parse.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+require 'set'
+
 if ARGV.length != 1
     puts "We need exactly one parameter. The name of a file."
     exit;
@@ -8,26 +10,29 @@ filename = ARGV[0]
 puts "Going to open '#{filename}'"
 file = open filename
 
-require 'rspec'
-
 class Log
   def parse(content)
     counts = {}
     content.each_line do |line|
-      page, _ = line.split
+      url, ip = line.split
 
-      # Count each webpage views
-      if counts[page]
-        counts[page] += 1
+      # Count visits and unique views of each page
+      # {"url": [visits, unique_views]}
+      if counts[url]
+        counts[url][0] += 1
+        counts[url][1].add(ip)
       else
-        counts[page] = 1
+        counts[url] = [1, Set[ip]]
       end
     end
     # Sort page visits in descending order
-    counts.sort_by { |k, v| -v }
+    sorted = counts.sort_by { |k, v| -v[0] }
+    # Print the result
+    sorted.each do |k, v|
+      puts "#{k} has #{v[0]} visits and #{v[1].length} unique views."
+    end
   end
 end
 
-result = Log.new.parse(file)
-puts result
+Log.new.parse(file)
 
