a2b2dd829edbadf727740b0b907d84dcc982c26b
digitaldiver <1119han@gmail.com>
2022-11-19 16:20:04 +0100

    add a new print for unique views

--- parse.rb
+++ parse.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby
 require 'set'
 
-if ARGV.length != 1
-    puts "We need exactly one parameter. The name of a file."
-    exit;
-end
- 
-filename = ARGV[0]
-puts "Going to open '#{filename}'"
-file = open filename
+class Parse
+  attr_accessor :filename
+  
+  def initialize(argv)
+    @filename = argv[0]
+    puts "Going to open '#{@filename}'"
+  end
 
-class Log
-  def parse(content)
+  def parse
+    file = open @filename
     counts = {}
-    content.each_line do |line|
+
+    file.each_line do |line|
       url, ip = line.split
 
       # Count visits and unique views of each page
@@ -25,14 +25,25 @@ class Log
         counts[url] = [1, Set[ip]]
       end
     end
-    # Sort page visits in descending order
-    sorted = counts.sort_by { |k, v| -v[0] }
-    # Print the result
-    sorted.each do |k, v|
-      puts "#{k} has #{v[0]} visits and #{v[1].length} unique views."
+
+    # Print the page visits result
+    sorted_visits = counts.sort_by { |k, v| -v[0] }
+    sorted_visits.each do |k, v|
+      puts "#{k} has #{v[0]} visits."
+    end
+
+    # Print the unique views result
+    unique_views = counts.sort_by { |k, v| -v[1].length }
+    unique_views.each do |k, v|
+      puts "  #{k} has #{v[1].length} unique views."
     end
   end
 end
 
-Log.new.parse(file)
-
+if ARGV.length != 1
+  puts "We need exactly one parameter. The name of a file."
+  exit;
+else
+  Parse.new(ARGV).parse
+end
+ 
